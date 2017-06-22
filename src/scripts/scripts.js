52a7ae050921ab67f16bae1187d3f3421f8868cb
Martin <martin@hackandcraft.com>
2017-06-22 12:04:01 +0200

    styles, analytics

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,3 +1,12 @@
 $(function(){
   $('body').scrollspy({ target: '#major-navbar' })
+
+  $("#major-navbar.smooth-scrollable").on('click', 'a', function(event){
+    event.preventDefault();
+    $('#major-navbar').collapse('hide')
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+});
 });
